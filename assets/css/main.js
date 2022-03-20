ec58f0b0f9b0a405c76b8b89ce33d7a3f1275aee
Hoangquan26 <101984807+Hoangquan26@users.noreply.github.com>
2022-03-20 22:37:14 +0700

    upload

---
+++ assets/css/main.js
@@ -0,0 +1,46 @@
+var btns = document.querySelectorAll('.js-buy');
+var length = btns.length;
+var modal = document.querySelector('.js-modal')
+var close = document.querySelectorAll('.js-close')
+var modalContain = document.getElementById('js-modal-content')
+for(var i = 0; i < length; i++) {
+    btns[i].addEventListener('click', openModal)
+}
+
+for(var i = 0; i < length - 1; i++) {
+    close[i].addEventListener('click', closeModal)
+}
+
+function openModal() {
+    modal.classList.add('open')
+}
+
+function closeModal() {
+    modal.classList.remove('open')
+}
+
+modal.addEventListener('click', closeModal);
+
+modalContain.addEventListener('click', function(e) {
+    console.log(e)
+    e.stopPropagation();
+})
+
+// responsive-menu
+var menuBtn = document.getElementById('menu-btn');
+var header = document.querySelector('.home')
+function openMenu() {
+    header.classList.toggle('menu')
+}
+menuBtn.addEventListener('click', openMenu)
+var more = document.querySelector('#nav>ul>li:last-child')
+var menus = document.querySelectorAll('#nav>ul>li');
+for(var i = 0; i < menus.length; i++) {
+    if(menus[i] != more)
+        menus[i].addEventListener('click', openMenu)
+    else
+        menus[i].addEventListener('click', function(e) {
+            e.preventDefault();
+        })
+}
+
